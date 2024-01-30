7e61e63e8aa4844a39d3c9f7435409cbb96d5858
drgnman <drgnman@ht.sfc.keio.ac.jp>
2024-01-30 16:14:48 +0900

    ファイル入力から出力まで実装

--- stretch_emotion/debug.py
+++ stretch_emotion/debug.py
@@ -5,18 +5,23 @@ from FileOperator import FileOperator
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_filename", help="input file name")
-# parser.add_argument("output_filepath", help="output file path")
+parser.add_argument("output_filepath", help="output file path")
 
 args = parser.parse_args()
 
+# ファイル操作用、感情変換用のクラスオブジェクトを生成
 fo = FileOperator()
 core = Core()
-
+ 
+# ファイルの読み込みと、出力リスト用のハードコピー
 datalist = fo.readCSVFile(args.input_filename)
 output_datalist = copy.copy(datalist)
 
+
+# 感情変換
 for i in range(len(datalist)):
     output_datalist[i][1], output_datalist[i][2] = core.stretchEmotion(args.input_filename, datalist[i][1], datalist[i][2])
     print("発話ID: ", output_datalist[i][0],"調整後の感情ID: ", output_datalist[i][1], "調整後の感情名: ", output_datalist[i][2])
 
-# emotion_id, emotion_name = core.stretchEmotion("テスト感情3", 1, "覚醒")
+# 結果書き出し
+fo.writeFileAllData(args.output_filepath, output_datalist)
